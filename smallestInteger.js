ad39416dbc4f0746e611b7f78a5b2a92ff47c2fd
rubenaguilardev <ragui56@wgu.edu>
2025-01-17 23:41:34 -0800

    Find the smallest integer in the array soluction

---
+++ smallestInteger.js
@@ -0,0 +1,21 @@
+/* 
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+    Given [34, 15, 88, 2] your solution will return 2
+    Given [34, -345, -1, 100] your solution will return -345
+
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+
+function findSmallestInt(arr) {
+    let smallest = arr[0]
+    for (let num = 0; num < arr.length; num++) {
+      if (arr[num] < smallest) {
+        smallest = arr[num]
+      } 
+    }
+    return smallest;
+  }
