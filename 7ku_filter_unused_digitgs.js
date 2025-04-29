dcfc0767f3fea1a69a6a068435c8af347bc18f9a
rubenaguilardev <ragui56@wgu.edu>
2025-04-29 01:34:22 -0700

    filter unused digits solution

---
+++ 7ku_filter_unused_digitgs.js
@@ -0,0 +1,18 @@
+// Given a varying number of integer arguments, return the digits that are not present in any of them.
+
+// Example:
+
+// [12, 34, 56, 78]  =>  "09"
+// [2015, 8, 26]     =>  "3479"
+
+// Note: the digits in the resulting string should be sorted.
+
+
+function unusedDigits(...nums) {
+    const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    const numbers = nums.join("")
+   
+     
+     return digits.filter((digit) => !numbers.includes(digit)).join("")
+       
+   }
